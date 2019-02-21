33893225b65d83dd7b8586d2c037d05f8f86962b
Cristiano Vieira <47859813+profcristianovs@users.noreply.github.com>
2019-02-21 14:32:30 -0300

    Update exemplo3.js

--- exemplo3.js
+++ exemplo3.js
@@ -6,7 +6,7 @@
    response.setEncoding('utf8');
    response.writeHead(200, {"Content-Type": "text/html; charset=UTF-8;"});
    response.write("<html><body>");
-   response.write("<h1>Dados da query string olá</h1>");
+   response.write("<h1>Recuperando valores por GET na URL</h1>");
    // Itera o resultado de parâmetros passados via query string.
    for(var key in result.query){
      response.write("<h2>"+key+" : "+result.query[key]+"</h2>");
