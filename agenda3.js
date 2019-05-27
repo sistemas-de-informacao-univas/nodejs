d858fb92d5060a6900f12e3dd533826e667f2244
Cristiano Vieira <47859813+profcristianovs@users.noreply.github.com>
2019-05-27 20:01:41 -0300

    Atualização da Agenda com a função Editar

---
+++ agenda3.js
@@ -0,0 +1,148 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
+var mysqlSync = require('sync-mysql');
+
+var app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+var con = mysql.createConnection({
+	host: '127.0.0.1',
+	user: 'root',
+	password: '',
+	database: 'univas'
+});
+
+var conSync = new mysqlSync({
+	host: '127.0.0.1',
+	user: 'root',
+	password: '',
+	database: 'univas'
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Conectado ao Banco de Dados");
+});
+
+app.get('/', function(req, res){
+	var html;
+	html = "<html><head>";
+	html += "<link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'>";
+	html += "</head><body>";
+	html += "<div align = 'center' width='50%'>";
+	html += "<form action='inserir' method='post'><br>";
+	html += "<h1>Controle da Agenda de Clientes</h1><br>";
+	html += " Nome: <input type='text' name='nome' > ";
+	html += " Telefone: <input type='text' name='telefone'> ";
+	html += " <input type='submit' value='Inserir'>";
+	html += "<hr>";
+	html += "<table border=1>";
+	html += "<thead>";
+	html += "<td width='40%'>NOME</td><td width='30%'>TELEFONE</td><td width='30%'>AÇÕES</td>";
+	html += "</thead>";
+
+	var sql = "SELECT * FROM agenda";
+
+	var rows = conSync.query(sql);
+	for(var i = 0; i < rows.length; i++){
+		html += "<tbody>";
+		html += "<td>"+ rows[i].nome + "</td>";
+		html += "<td>"+ rows[i].telefone + "</td>";
+		html += "<td><a href='/" + rows[i].id + "'>Editar</a>";
+		html += "	 <a href='/remover/" + rows[i].id + "'>Remover</a></td>";
+
+		html += "</tbody>";
+	};
+
+	html += "</table></div>";
+	html += "</body></html>";
+	res.send(html);
+
+});
+
+
+app.post('/inserir', function(req, res){
+	var post = {
+        nome: req.body.nome,
+        telefone: req.body.telefone
+    };
+	
+    var sql = "INSERT INTO agenda SET ?";
+
+	con.query(sql, [post], function(erro){
+		if (erro) {
+	        console.log(erro.message);
+	    } else {
+			console.log("Dados inseridos na agenda");
+		}
+	});
+	res.redirect('/');
+
+});
+
+
+app.get('/:id', function(req, res) {
+	
+	var id = req.params.id;
+
+	var sql = "SELECT * FROM agenda WHERE id = " + id;
+
+	var rows = conSync.query(sql);
+	var nome = rows[0].nome;
+	var telefone = rows[0].telefone;
+	
+	var html;
+	html = "<html><head>";
+	html += "<link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'>";
+	html += "</head><body>";
+	html += "<div align = 'center' width='50%'>";
+	html += "<form action='atualizar' method='post'><br>";
+	html += "<h1>Editando dados na Agenda de Clientes</h1><br>";
+	html += " Nome: <input type='text' name='nome' value='" + nome + "'> ";
+	html += " Telefone: <input type='text' name='telefone' value='" + telefone + "'> ";
+	html += " <input type='hidden' name='id' value='" + id + "'>";
+	html += " <input type='submit' value='Atualizar'>";
+
+	res.send(html);
+
+});
+
+
+app.post('/atualizar', function(req, res){
+	var post = {
+	    nome: req.body.nome,
+	    telefone: req.body.telefone
+	};
+
+	var id = req.body.id;
+ 
+	con.query('UPDATE agenda set ? where id = ?', [post, id], function(erro, result) {
+	    if (erro) {
+	        console.log(erro.message);
+	    } else {
+	        console.log('Registro atualizado com sucesso!');
+	    }
+    });
+	res.redirect('/')
+});
+
+
+app.get('/remover/:id', function(req, res) {
+	
+	var id = req.params.id;
+
+    con.query('DELETE FROM agenda WHERE id = ' + id, function(erro, result) {
+	    if (erro) {
+	        console.log(erro.message);
+	    } else {
+	        console.log('Registro removido com sucesso!');
+	    }
+    });
+	res.redirect('/')
+});
+
+
+app.listen(3000, function(){
+	console.log('Servidor pronto');
+});
